555771f2723d33caca56e534e6f17cabb1bc4f3d
Matt <39469614+KosmicShovel@users.noreply.github.com>
2021-06-21 05:45:23 -0500

    forgot to put this with the previous one

---
+++ BTDOptimizations/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using MelonLoader;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BTD6 Optimizer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("1330 Studios LLC")]
+[assembly: AssemblyProduct("BTD6 Optimizer")]
+[assembly: AssemblyCopyright("Copyright © 1330 Studios LLC 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("307ae887-7dca-43ef-a7c0-a112d4bf2061")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: @MelonPriority(int.MaxValue)]
+[assembly: MelonColor(ConsoleColor.DarkYellow)]
